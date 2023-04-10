2fb59be68f20286b1cdfc186cda4132a83628a19
Poojamhetre <mhetrepooja23@gmail.com>
2023-04-10 15:13:28 -0400

    Update Placing_aliens.py

--- Placing_aliens.py
+++ Placing_aliens.py
@@ -6,7 +6,7 @@ import pygame
 class Alien(pygame.sprite.Sprite):
 	def __init__(self,color,x,y):
 		super().__init__()
-		file_path = '../graphics/' + color + '.png'
+		file_path = '../Images/' + color + '.png'
 		self.image = pygame.image.load(file_path).convert_alpha()
 		self.rect = self.image.get_rect(topleft = (x,y))
 
